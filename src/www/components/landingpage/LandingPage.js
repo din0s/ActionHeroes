1a21a9b6dd3ba410e51a43e3831fe0e5c3561e7c
SotirisFtiakas <sftiakas@csd.auth.gr>
2020-03-19 06:32:06 +0200

    Scroll-Up Button Added

--- src/www/components/landingpage/LandingPage.js
+++ src/www/components/landingpage/LandingPage.js
@@ -4,16 +4,18 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Parallax } from 'react-parallax';
+import ScrollUpButton from "react-scroll-up-button";
 
+const bgkidimg = '/img/KidBackground.png';
 const bgimg = '/img/background.jpg';
 const innerimg = './img/volunteer.svg';
 
 
-export default function LandingPage() {
+export default function LandingPage() {       //I need to fill the "alt"s
   const { t } = useTranslation();
   return (
     <div className='container'>
-      <Parallax className='parallax' bgImage={bgimg} bgImageAlt="the alt" strength={800}>
+      <Parallax className='parallax' bgImage={bgkidimg} bgImageAlt="the alt" strength={500}>
         <div className='panel'>
           <img className='innerphoto' src={innerimg} />
           <div className='innertextcontainer'>
@@ -22,10 +24,23 @@ export default function LandingPage() {
             <Link className="joinlink" to="/signup"><button class="joinbutton">{t("landingpage.join")}</button></Link>
           </div>
         </div>
-
+        <ScrollUpButton ContainerClassName='scrollbutton' AnimationDuration={1200}
+         EasingType='easeInOutCubic' ShowAtPosition={150}/>
       </Parallax>
       <div className='test'>Testing Block</div>
       <Parallax className='parallax' bgImage={bgimg} bgImageAlt="the alt" strength={600}>
+        For testing purposes only. Will remove later.
+      </Parallax>
+      <div className='test'>Testing Block</div>
+      <Parallax className='parallax' bgImage={bgimg} bgImageAlt="the alt" strength={800}>
+        For testing purposes only. Will remove later.
+      </Parallax>
+      <div className='test'>Testing Block</div>
+      <Parallax className='parallax' bgImage={bgimg} bgImageAlt="the alt" strength={600}>
+        For testing purposes only. Will remove later.
+      </Parallax>
+      <div className='test'>Testing Block</div>
+      <Parallax className='parallax' bgImage={bgimg} bgImageAlt="the alt" strength={800}>
         For testing purposes only. Will remove later.
       </Parallax>
     </div>
