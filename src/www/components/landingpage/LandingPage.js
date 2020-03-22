455c8cc8d9a618c9a332660198c79222b3324de1
SotirisFtiakas <sftiakas@csd.auth.gr>
2020-03-22 06:25:07 +0200

    Landing-Page structure finished, needs responsiveness

--- src/www/components/landingpage/LandingPage.js
+++ src/www/components/landingpage/LandingPage.js
@@ -10,12 +10,14 @@ import { useTranslation } from "react-i18next";
 const bgkidimg = '/img/KidBackground.png';
 const bgimg = '/img/background.jpg';
 const innerimg = './img/volunteer.svg';
+const categories = {category:"Category", image:"./img/background.jpg", desc:"Description Desc ription Descri ption Des c ription Descrip tion."}
 
 
 export default function LandingPage() {       //I need to fill the "alt"s
   const { t } = useTranslation();
   return (
     <div className='container'>
+
       <Parallax className='parallax' bgImage={bgkidimg} bgImageAlt="the alt" strength={500}>
         <div className='panel'>
           <img className='innerphoto' src={innerimg} />
@@ -29,16 +31,17 @@ export default function LandingPage() {       //I need to fill the "alt"s
           EasingType='easeInOutCubic' ShowAtPosition={150} />
       </Parallax>
 
+
       <div className='secondpage'>
         <p className='phrase' >Become an Action Hero</p>    
         <Parallax blur={0.5} className='parallaxbg' bgImage={bgkidimg} bgImageAlt="the alt" strength={200}>
           <div className='panel'>
-            <div className='box'>
+            <div className='card'>
               <p className='boxphrase'>Volunteer</p>
               <img className='boximg' src={bgimg} />
               <p className='boxdesc'>Lorem ipsum dolor sit amet. Duis neque nisl, tincidunt quis lorem vel, efficitur luctus orci. Vivamus dignissim tincidunt erat ac semper.</p>
             </div>
-            <div className='box'>
+            <div className='card'>
               <p className='boxphrase'>Organise</p>
               <img className='boximg' src={bgimg} />
               <p className='boxdesc'>Lorem ipsum dolor sit amet. Duis neque nisl, tincidunt quis lorem vel, efficitur luctus orci. Vivamus dignissim tincidunt erat ac semper.</p>
@@ -46,9 +49,57 @@ export default function LandingPage() {       //I need to fill the "alt"s
           </div>
         </Parallax>
       </div>
-      {/* <Parallax className='parallax' bgImage={bgimg} bgImageAlt="the alt" strength={800}>
-        For testing purposes only. Will remove later.
-      </Parallax> */}
+
+
+      <div className='thirdpage'>
+        <p className='phrase' >You can find events & actions for:</p>    
+        <Parallax blur={0.5} className='parallaxbg' bgImage={bgkidimg} bgImageAlt="the alt" strength={200}>
+          <div className='panel'>
+            <div className='card'>
+              <div className='box'>
+                <img className='boximg' src={categories.image} />
+                <p className='category'>{categories.category}</p>
+              </div>
+              <p className='boxdesc'>{categories.desc}</p>
+            </div>
+            <div className='card'>
+              <div className='box'>
+                <img className='boximg' src={categories.image} />
+                <p className='category'>{categories.category}</p>
+              </div>
+              <p className='boxdesc'>{categories.desc}</p>
+            </div>
+            <div className='card'>
+              <div className='box'>
+                <img className='boximg' src={categories.image} />
+                <p className='category'>{categories.category}</p>
+              </div>
+              <p className='boxdesc'>{categories.desc}</p>
+            </div>
+            <div className='card'>
+              <div className='box'>
+                <img className='boximg' src={categories.image} />
+                <p className='category'>{categories.category}</p>
+              </div>
+              <p className='boxdesc'>{categories.desc}</p>
+            </div>
+            <div className='card'>
+              <div className='box'>
+                <img className='boximg' src={categories.image} />
+                <p className='category'>{categories.category}</p>
+              </div>
+              <p className='boxdesc'>{categories.desc}</p>
+            </div>
+            <div className='card'>
+              <div className='box'>
+                <img className='boximg' src={categories.image} />
+                <p className='category'>{categories.category}</p>
+              </div>
+              <p className='boxdesc'>{categories.desc}</p>
+            </div>
+          </div>
+        </Parallax>
+      </div>
     </div>
   )
 };
