c1b8a20ec63b11813d55fd754c8278149bc532ab
GregB712 <gregorybarbas@gmail.com>
2020-03-22 18:28:43 +0200

    Add responsiveness

--- src/www/components/contactpage/ContactPage.js
+++ src/www/components/contactpage/ContactPage.js
@@ -12,12 +12,12 @@ export default function ContactPage() {
     const { t } = useTranslation();
     return (
         <div className="container">
-            <div className="title-block">
-                <img className="top-img" src={top_img}/>
+            
+                <img className="top-img" alt="img1" src={top_img}/>
                 <span className="contact-form-title1">{t("contactpage.title1")}</span>
                 <span className="contact-form-title2">{t("contactpage.title2")}</span>
                 <div className="contact-form-block">
-                    <img className="form-image" src={form_img}/>
+                    <img className="form-image" alt="img2" src={form_img}/>
                     <form className="contact-form">
                         <input type="text" placeholder={t("contactpage.first_name")} />
                         <input type="text" placeholder={t("contactpage.last_name")} />
@@ -26,7 +26,7 @@ export default function ContactPage() {
                         <button className="submit-btn" type="submit">{t("contactpage.submit")}</button>
                     </form>
                 </div>
-            </div>
+            
         </div>
     )
   };
