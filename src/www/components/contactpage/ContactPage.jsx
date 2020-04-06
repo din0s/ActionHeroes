904849fb05839ef8f1d412758d0982b9b239ee12
GregB712 <gregorybarbas@gmail.com>
2020-04-06 19:09:53 +0300

    Code beautification & image uploading

--- src/www/components/contactpage/ContactPage.jsx
+++ src/www/components/contactpage/ContactPage.jsx
@@ -6,12 +6,8 @@ import { clearErrors, contact } from "../../actions/contact";
 import { connect } from "react-redux";
 import { withTranslation } from "react-i18next";
 
-//import axios from "axios";
-
-
-
-const top_img = '/img/keyboard.png';
-const form_img = '/img/inner.png'
+const top_img = '/img/top_img.png'; //Photo by Patrick Perkins on Unsplash
+const form_img = '/img/form_img.png'; //Photo by LinkedIn Sales Navigator on Unsplash
 
 const mapState = state => ({
   error: state.auth.error
@@ -63,8 +59,9 @@ class ContactPage extends Component {
     event.preventDefault();
     this.props.clearErrors();
     const data = new FormData(event.target);
-    //console.log(stringifyFormData(data))
+    console.log(stringifyFormData(data))
 
+    //alert(stringifyFormData(data));
     this.props.contact(data.get('first_name'), data.get('last_name'), data.get('email'), data.get('question'));
     this.setState({ first_name: '', last_name: '', email: '', question: '' })
   }
@@ -73,49 +70,49 @@ class ContactPage extends Component {
       const { t } = this.props;
       return (
         <div className="container">    
-            <img className="top-img" alt="img1" src={top_img}/>
+            <img className="top-img" alt="top_img" src={top_img}/>
             <span className="contact-form-title1">{t("contactpage.title1")}</span>
             <span className="contact-form-title2">{t("contactpage.title2")}</span>
             <div className="contact-form-block">
-                <img className="form-image" alt="img2" src={form_img}/>
+                <img className="form-image" alt="form_img" src={form_img}/>
                 <form onSubmit={this.handleSubmit} >
-                    <input
-                        name="first_name" 
-                        type='text'
-                        value={this.state.first_name}
-                        onChange={this.onChangeFirstName}
-                        required
-                        placeholder={t("contactpage.first_name")} 
-                    />
-                    <input
-                        name="last_name" 
-                        type='text'
-                        value={this.state.last_name}
-                        onChange={this.onChangeLastName}
-                        required
-                        placeholder={t("contactpage.last_name")} 
-                    />
-                    <input
-                        name="email" 
-                        type='email'
-                        value={this.state.email}
-                        onChange={this.onChangeEmail}
-                        required
-                        placeholder={t("contactpage.email")}
-                    />
-                    <textarea 
-                        name="question"
-                        type='text'
-                        value={this.state.question}
-                        onChange={this.onChangeQuestion}
-                        required
-                        placeholder={t("contactpage.question")}
-                    />
-                    <input
-                        type="submit"
-                        className="submit-btn"
-                        value={t("contactpage.submit")}
-                    />
+                   <input
+                      name="first_name" 
+                      type='text'
+                      value={this.state.first_name}
+                      onChange={this.onChangeFirstName}
+                      required
+                      placeholder={t("contactpage.first_name")} 
+                  />
+                  <input
+                      name="last_name" 
+                      type='text'
+                      value={this.state.last_name}
+                      onChange={this.onChangeLastName}
+                      required
+                      placeholder={t("contactpage.last_name")} 
+                  />
+                  <input
+                      name="email" 
+                      type='email'
+                      value={this.state.email}
+                      onChange={this.onChangeEmail}
+                      required
+                      placeholder={t("contactpage.email")}
+                  />
+                  <textarea 
+                      name="question"
+                      type='text'
+                      value={this.state.question}
+                      onChange={this.onChangeQuestion}
+                      required
+                      placeholder={t("contactpage.question")}
+                  />
+                  <input
+                      type="submit"
+                      className="submit-btn"
+                      value={t("contactpage.submit")}
+                  />
                 </form>
             </div>
         </div>
