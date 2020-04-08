df7f405046d613b1a317ab2d83bd7f1225d1a96a
SotirisFtiakas <sftiakas@csd.auth.gr>
2020-04-08 11:23:39 +0300

    Added some pics and descriptions

--- src/www/components/landingpage/LandingPage.js
+++ src/www/components/landingpage/LandingPage.js
@@ -9,9 +9,11 @@ import { useTranslation } from "react-i18next";
 
 const bgkidimg = '/img/KidBackground.png';
 const bgimg = '/img/background.jpg';
-const innerimg = './img/volunteer.svg';
+const innerimg = './img/smallicon.svg';
 const categories = { category: "Category", image: "./img/background.jpg", desc: "Description Desc ription Descri ption Des c ription." }
 
+const categoriesList = '{ "Category1": { "image":"./img/background.jpg", "name": "Category1", "desc": "AAADescription Desc ription Descri ption Des c ription." }, "Category2": { "image": "./img/background.jpg", "name": "Category2", "desc": "Description Desc ription Descri ption Des c ription." } }'
+const JSONcat = JSON.parse(categoriesList)
 
 export default function LandingPage() {       //I need to fill the "alt"s
   const { t } = useTranslation();
@@ -38,13 +40,13 @@ export default function LandingPage() {       //I need to fill the "alt"s
           <div className='panel'>
             <div className='card'>
               <p className='boxphrase'>{t("landingpage.volunteer")}</p>
-              <img className='boximg' src={bgimg} />
+              <img className='boximg' src={"./img/volunteer.svg"} />
               <p className='boxdesc'>{t("landingpage.volunteerdesc")}</p>
             </div>
             <div className='card'>
-              <p className='boxphrase'>{t("landingpage.organise")}</p>
-              <img className='boximg' src={bgimg} />
-              <p className='boxdesc'>{t("landingpage.organisedesc")}</p>
+              <p className='boxphrase'>{t("landingpage.organize")}</p>
+              <img className='boximg' src={"./img/organizer.svg"} />
+              <p className='boxdesc'>{t("landingpage.organizedesc")}</p>
             </div>
           </div>
         </Parallax>
@@ -55,6 +57,18 @@ export default function LandingPage() {       //I need to fill the "alt"s
         <p className='phrase' >{t("landingpage.findactions")}</p>
         <Parallax blur={0.5} className='parallaxbg' bgImage={bgkidimg} bgImageAlt="the alt" strength={150}>
           <div className='panel'>
+
+            {/* {JSONcat.map((category) => {
+              return(
+                <div className='card'>
+                  <div className='box'>
+                    <img className='boximg' src={category.image} />
+                    <p className='category'>{category.name}</p>
+                  </div>
+                  <p className='boxdesc'>{category.desc}</p>
+                </div>);
+            })} */}
+
             <div className='card'>
               <div className='box'>
                 <img className='boximg' src={categories.image} />
@@ -69,14 +83,14 @@ export default function LandingPage() {       //I need to fill the "alt"s
               </div>
               <p className='boxdesc'>{categories.desc}</p>
             </div>
-             <div className='card'>
+            <div className='card'>
               <div className='box'>
                 <img className='boximg' src={categories.image} />
                 <p className='category'>{categories.category}</p>
               </div>
               <p className='boxdesc'>{categories.desc}</p>
             </div>
-            {/* <div className='card'>
+            <div className='card'>
               <div className='box'>
                 <img className='boximg' src={categories.image} />
                 <p className='category'>{categories.category}</p>
@@ -96,7 +110,7 @@ export default function LandingPage() {       //I need to fill the "alt"s
                 <p className='category'>{categories.category}</p>
               </div>
               <p className='boxdesc'>{categories.desc}</p>
-            </div> */}
+            </div>
           </div>
         </Parallax>
       </div>
