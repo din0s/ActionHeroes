db054f346f1b176272a5a42f2ba1f92f99dee2a0
dinos <papakosk@csd.auth.gr>
2020-04-13 17:17:26 +0300

    Heroku deployment support for review apps (#18)

--- src/api/index.js
+++ src/api/index.js
@@ -14,7 +14,7 @@ const userRoutes = require("./routes/user");
 const contactRoutes = require("./routes/contact");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // Database init connection
 const dbUser = process.env.MONGO_INITDB_ROOT_USERNAME;
@@ -23,11 +23,16 @@ const dbHost = process.env.MONGO_SERVICE_HOST;
 const dbPort = process.env.MONGO_SERVICE_PORT;
 const dbName = process.env.MONGO_INITDB_DATABASE;
 
+const dbUri =
+  process.env.MONGODB_URI ||
+  `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
+
 mongo
-  .connect(
-    `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-  )
+  .connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
   .then(() => console.log("Connected to database."))
   .catch(() => console.error("Could not connect to database!"));
 
@@ -42,13 +47,13 @@ morgan.token("ip", (req, res) => {
 
 var accessLogStream = rfs.createStream("access.log", {
   interval: "30d", // rotate monthly
-  path: path.join(process.cwd(), "log"),
+  path: path.join(process.cwd(), "log")
 });
 
 // Logging to file
 app.use(
   morgan("[:date[web]] :ip :method :url :status :body", {
-    stream: accessLogStream,
+    stream: accessLogStream
   })
 );
 
@@ -66,19 +71,26 @@ app.use("/teams", teamRoutes);
 app.use("/users", userRoutes);
 app.use("/contact", contactRoutes);
 
-/* Can't find the requested resourse */
-app.use((req, res, next) => {
-  const error = new Error("Resource not found");
-  error.status = 404;
-  next(error);
-});
+if (process.env.NODE_ENV == "production") {
+  // Serve static content in build directory
+  app.use(express.static(path.join(process.cwd(), "build")));
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(process.cwd(), "build", "index.html"));
+  });
+} else {
+  app.use((req, res, next) => {
+    const error = new Error("Resource not found");
+    error.status = 404;
+    next(error);
+  });
+}
 
-/* Any other error */
+// Any other error
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   if (error.status) {
     res.json({
-      error: error.message,
+      error: error.message
     });
   } else {
     res.send();
