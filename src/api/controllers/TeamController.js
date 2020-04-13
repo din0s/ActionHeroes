dcb76be05335b5f56b9f784ebf5af9950b725f60
Christina <chriskreza@csd.auth.gr>
2020-04-13 17:54:47 +0300

    Implemented teams/create endpoint

--- src/api/controllers/TeamController.js
+++ src/api/controllers/TeamController.js
@@ -1,7 +1,42 @@
+const Team = require("../models/TeamModel");
+
 module.exports = {
   addMembers: (req, res) => {},
 
-  createTeam: (req, res) => {},
+  createTeam: (req, res) => {
+    const name = req.body.name;
+
+    Team.find({ name })
+      .then(team => {
+        if (team.length == 1) {
+          return res.status(409).json({
+            message: "Name not available."
+          });
+        }
+
+        team = new Team({
+          owner: req.userData.userId
+        });
+
+        Team.schema.eachPath(path => {
+          if (req.body[path]) {
+            team[`${path}`] = req.body[path];
+          }
+        });
+
+        team
+          .save()
+          .then(() => res.status(201).send())
+          .catch(err => {
+            console.error(`Error during team save():\n${err}`);
+            res.status(400).json(err);
+          });
+      })
+      .catch(err => {
+        console.error(`Error during team find():\n${err}`);
+        res.status(500).send();
+      });
+  },
 
   deleteMembers: (req, res) => {},
 
