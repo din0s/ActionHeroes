7ac11c17d1546e6d95533f8bf12d9d2f24631dc9
dinos <papakosk@csd.auth.gr>
2020-04-21 20:32:35 +0300

    FilterList responsiveness for devices <800px (#33)

--- src/www/components/filterlist/FilterList.jsx
+++ src/www/components/filterlist/FilterList.jsx
@@ -6,6 +6,19 @@ import { useTranslation } from "react-i18next";
 const isChecked = (item, list) =>
   Object.keys(list).find((k) => list[k] === item);
 
+const listFilters = (t, selected, onRemove) => {
+  if (selected.length === 0) {
+    return <p children={t("filterlist.none")} />;
+  }
+
+  return selected.map((c) => (
+    <li key={c}>
+      <p children={c} />
+      <p onClick={() => onRemove(c)} children="x" />
+    </li>
+  ));
+};
+
 export default ({ categories, selected, onCheckbox, onClear, onRemove }) => {
   const { t } = useTranslation();
   return (
@@ -15,32 +28,27 @@ export default ({ categories, selected, onCheckbox, onClear, onRemove }) => {
           <h2>{t("filterlist.filters")}</h2>
           <p onClick={onClear}>{t("filterlist.clear")}</p>
         </span>
-        <ul>
-          {selected.map((c) => (
-            <li key={c}>
-              <p children={c} />
-              <p onClick={() => onRemove(c)} children="x" />
-            </li>
-          ))}
-        </ul>
+        <ul children={listFilters(t, selected, onRemove)} />
       </div>
       <div className="FilterList_menu">
         <h4>{t("filterlist.categories")}</h4>
-        {Object.keys(categories).map((c) => {
-          const category = categories[c];
-          const name = category.name;
-          return (
-            <label key={c}>
-              {name}
-              <input
-                type="checkbox"
-                checked={isChecked(name, selected)}
-                onChange={(event) => onCheckbox(event, name)}
-              />
-              <span className="checkmark"></span>
-            </label>
-          );
-        })}
+        <div>
+          {Object.keys(categories).map((c) => {
+            const category = categories[c];
+            const name = category.name;
+            return (
+              <label key={c}>
+                {name}
+                <input
+                  type="checkbox"
+                  checked={isChecked(name, selected)}
+                  onChange={(event) => onCheckbox(event, name)}
+                />
+                <span className="checkmark"></span>
+              </label>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
