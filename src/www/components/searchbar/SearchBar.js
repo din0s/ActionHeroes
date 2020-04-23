4e74a470e4c4ebd0398d90f67ab847ec627e011c
Dinos Papakostas <papakosk@csd.auth.gr>
2020-04-23 22:40:18 +0300

    Add search functionality

--- src/www/components/searchbar/SearchBar.js
+++ src/www/components/searchbar/SearchBar.js
@@ -3,11 +3,17 @@ import "./SearchBar.scss";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-export default (props) => {
+export default ({ action, value, onChange }) => {
   const { t } = useTranslation();
   return (
-    <form action={props.action} method="get" className="SearchBar">
-      <input type="text" name="q" placeholder={t("search")} />
+    <form action={action} method="get" className="SearchBar">
+      <input
+        type="text"
+        name="q"
+        placeholder={t("search")}
+        defaultValue={value}
+        onChange={onChange}
+      />
     </form>
   );
 };
--- src/www/containers/teamspage/TeamsPage.jsx
+++ src/www/containers/teamspage/TeamsPage.jsx
@@ -4,15 +4,24 @@ import React, { Component } from "react";
 
 import FilterList from "../../components/filterlist/FilterList";
 import SearchBar from "../../components/searchbar/SearchBar";
-import { withTranslation } from "react-i18next";
+import queryString from "query-string";
+import { withRouter } from "react-router-dom";
 
 const categories = require("./categories.json");
 const teams = require("./teams.json");
 
-export default withTranslation()(
+export default withRouter(
   class TeamsPage extends Component {
     state = {
       selectedCategories: [],
+      query: "",
+    };
+
+    componentDidMount = () => {
+      const { q } = queryString.parse(this.props.location.search);
+      if (q) {
+        this.setState({ query: q.toLowerCase().trim() });
+      }
     };
 
     onCheckbox = (event, category) => {
@@ -34,7 +43,18 @@ export default withTranslation()(
     };
 
     showTeams = () => {
+      const q = this.state.query;
       return Object.keys(teams)
+        .filter((tm) => {
+          if (q === "") {
+            return true;
+          }
+          const team = teams[tm];
+          return (
+            team.name.toLowerCase().includes(q) ||
+            team.description.toLowerCase().includes(q)
+          );
+        })
         .filter((tm) => {
           const selected = this.state.selectedCategories;
           if (selected.length === 0) {
@@ -64,10 +84,15 @@ export default withTranslation()(
     };
 
     render() {
-      // const { t } = this.props;
       return (
         <div className="TeamsPage">
-          <SearchBar action="/teams" />
+          <SearchBar
+            action="/teams"
+            value={this.state.query}
+            onChange={(e) =>
+              this.setState({ query: e.target.value.toLowerCase().trim() })
+            }
+          />
           <span>
             <FilterList
               categories={categories}
