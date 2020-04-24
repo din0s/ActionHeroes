91041ff103a65b6b525d608fc2978397a2614310
SotirisFtiakas <sftiakas@csd.auth.gr>
2020-04-24 18:19:16 +0300

    ActionCard ready

--- src/www/components/actioncard/ActionCard.jsx
+++ src/www/components/actioncard/ActionCard.jsx
@@ -5,17 +5,18 @@ import { withTranslation } from "react-i18next";
 
 const action = require("./action.json");
 
-function add3Dots(string, limit) {
-  var dots = "...";
-  if (string.length > limit) {
-    string = string.substring(0, limit) + dots;
-  }
-
-  return string;
-}
-
 export default withTranslation()(
   class ActionCard extends Component {
+    parseDate = (date) => {
+      const { t } = this.props;
+      const d = new Date(date);
+      const day = t("actioncard.date.day." + d.getDay());
+      const month = t("actioncard.date.month." + d.getMonth());
+
+      return `${day}, ${d.getDate()} ${month} ${d.getFullYear()}, 
+      ${d.getUTCHours()}:${d.getUTCMinutes()}`;
+    };
+
     render() {
       const { t } = this.props;
       return (
@@ -23,14 +24,20 @@ export default withTranslation()(
           <img src={action.photo} alt="" />
           <div className="Card_body">
             <h1>{action.name}</h1>
-            <h2>📍 Thessaloniki, Greece</h2>
-            <h3>ACM AUTH Student Chapter</h3>
-            <p>{add3Dots(action.description, 100)}</p>
+            <h2>
+              📍 {action.location.name} • {this.parseDate(action.date)}
+            </h2>
+            {/* <h3>{action.organizer.teamId.name}</h3> */}
+            <p>{action.description}</p>
             <div className="Card_categories">
               <h1>{t("actioncard.categories")}:</h1>
               <ul>
                 {Object.keys(action.categories).map((cKey) => {
-                  return <li>{action.categories[cKey].name}</li>;
+                  return (
+                    <li>{`${t(
+                      "categories." + action.categories[cKey].name
+                    )}`}</li>
+                  );
                 })}
               </ul>
             </div>
