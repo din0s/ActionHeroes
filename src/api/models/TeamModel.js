39017d51867906910eb03a897d953d4e8674e351
Christina <chriskreza@csd.auth.gr>
2020-04-24 18:33:27 +0300

    Fixed rebase

--- src/api/models/TeamModel.js
+++ src/api/models/TeamModel.js
@@ -4,18 +4,11 @@ const uniqueValidator = require("mongoose-unique-validator");
 const TeamSchema = new mongo.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String },
-<<<<<<< HEAD
-  logo: { data: Buffer, contentType: String },
+  photo: { data: Buffer, contentType: String },
   owner: { type: mongo.Schema.Types.ObjectId, required: true, ref: "User" },
   members: [{ type: mongo.Schema.Types.ObjectId, ref: "User" }],
   categories: [{ type: mongo.Schema.Types.ObjectId, ref: "Category" }],
   dateCreated: { type: Date, default: Date.now },
-=======
-  photo: { type: String },
-  owner: { type: mongo.Schema.Types.ObjectId, required: true },
-  members: { type: [mongo.Schema.Types.ObjectId] },
-  categories: { type: [String] }
->>>>>>> bb8f411... Team tab
 });
 
 TeamSchema.plugin(uniqueValidator);
