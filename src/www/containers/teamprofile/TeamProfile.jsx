37d371487540e2c3eeaa44c890724dbcadff1bf6
SotirisFtiakas <sftiakas@csd.auth.gr>
2020-04-24 23:17:12 +0300

    Only responsiveness left

--- src/www/containers/teamprofile/TeamProfile.jsx
+++ src/www/containers/teamprofile/TeamProfile.jsx
@@ -5,10 +5,23 @@ import { Link } from "react-router-dom";
 import { withTranslation } from "react-i18next";
 import ActionCard from "../../components/actioncard/ActionCard";
 
-const team = require("./teamobject.json");
+const team = require("./team.json");
 
 export default withTranslation()(
   class LandingPage extends Component {
+    state = {
+      followed: false,
+      totalmembers: team.followers.length,
+    };
+
+    parseDate = (date) => {
+      const { t } = this.props;
+      const d = new Date(date);
+      const day = t("actioncard.date.day." + d.getDay());
+      const month = t("actioncard.date.month." + d.getMonth());
+
+      return `${d.getDate()} ${month} ${d.getFullYear()}`;
+    };
     render() {
       const { t } = this.props;
       return (
@@ -17,47 +30,68 @@ export default withTranslation()(
             <span className="TopPanel_info">
               <img alt="Team Logo" src="/img/teaminfo/acm.jpg" />
               <div>
-                <h1>ACM AUTH Student Chapter</h1>
-                <h2>
-                  The Chapter's mission is to inform its members and students of
-                  Aristotle University of Thessaloniki (AUTh) about the latest
-                  cutting-edge achievements in Computer Science and Engineering,
-                  to create a group of dedicated people who are interested to
-                  explore all possibilities of computer science.
-                </h2>
+                <h1>{team.name}</h1>
+                <h2>{team.description}</h2>
               </div>
             </span>
-            <span className="TopPanel_button">
-              <Link to="/signup">
-                <button>{"Follow"}</button>
-              </Link>
-            </span>
+            {!this.state.followed && (
+              <button
+                className="TopPanel_followbutton"
+                onClick={() =>
+                  this.setState({
+                    followed: !this.state.followed,
+                    totalmembers: this.state.totalmembers + 1,
+                  })
+                }
+                children={t("teaminfo.follow")}
+              />
+            )}
+            {this.state.followed && (
+              <button
+                className="TopPanel_unfollowbutton"
+                onClick={() =>
+                  this.setState({
+                    followed: !this.state.followed,
+                    totalmembers: this.state.totalmembers - 1,
+                  })
+                }
+                children={t("teaminfo.unfollow")}
+              />
+            )}
           </section>
           <section className="BottomPanel">
             <div className="LeftSide">
               <div className="LeftSide_info">
-                <h1>Members: 75</h1>
-                <h1>Actions: 40</h1>
+                <h1>
+                  {t("teaminfo.members")}: {this.state.totalmembers}
+                </h1>
+                <h1>
+                  {t("teaminfo.actions")}: {Object.keys(team.actionList).length}
+                </h1>
                 <hr />
-                <h1>Created on:</h1>
-                <h1>September 14th 2019</h1>
+                <h1>{t("teaminfo.createdon")}:</h1>
+                <h1>{this.parseDate(team.dateCreated)}</h1>
               </div>
 
               <div className="LeftSide_categories">
-                <h1>Relevant Categories:</h1>
+                <h1>{t("teaminfo.categories")}:</h1>
                 <ul>
                   {Object.keys(team.categories).map((cKey) => {
-                    return <li>{team.categories[cKey].name}</li>;
+                    return (
+                      <li>{`${t(
+                        "categories." + team.categories[cKey].name
+                      )}`}</li>
+                    );
                   })}
                 </ul>
               </div>
             </div>
             <div className="RightSide">
               <div className="RightSide_events">
-                <h1>Upcoming Events:</h1>
+                <h1>{t("teaminfo.upcoming")}:</h1>
                 <ActionCard />
                 <ActionCard />
-                <h1>Past Events:</h1>
+                <h1>{t("teaminfo.past")}:</h1>
                 <ActionCard />
                 <ActionCard />
               </div>
