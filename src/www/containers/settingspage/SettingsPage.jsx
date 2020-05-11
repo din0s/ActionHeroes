20299484b7c0771245edb7137ce887af3b8028cc
GregB712 <gregorybarbas@gmail.com>
2020-05-12 00:26:19 +0300

    Basic Structure of Settings Page

--- src/www/containers/settingspage/SettingsPage.jsx
+++ src/www/containers/settingspage/SettingsPage.jsx
@@ -2,13 +2,23 @@ import "./SettingsPage.scss";
 
 import React, { Component } from "react";
 
-import Input from "../../components/input/Input";
 import { Redirect } from "react-router-dom";
+import Select from "react-select";
 import { connect } from "react-redux";
+import makeAnimated from "react-select/animated";
+import { render } from "react-dom";
 import { withTranslation } from "react-i18next";
 
 //const favoutite = require("./favourite.json");
 //const categories = require("./categories.json");
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
+const animatedComponents = makeAnimated();
 
 const mapState = (state) => ({
   user: state.auth.user,
@@ -21,10 +31,18 @@ export default connect(
 )(
   withTranslation()(
     class SettingsPage extends Component {
-      state = { activeTab: "security" };
+      state = {
+        activeTab: "profile",
+        selectedOption: null,
+      };
 
       submit = async (event) => {
         event.preventDefault();
+      };
+
+      handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+        console.log(`Option selected:`, selectedOption);
       };
 
       render() {
@@ -36,6 +54,7 @@ export default connect(
         const { t, user } = this.props;
         const { username, profilePhoto } = user;
         const photo = profilePhoto || "/img/fakedata/profilePhoto.png";
+        const { selectedOption } = this.state;
 
         return (
           <div className="SettingsPage">
@@ -65,62 +84,112 @@ export default connect(
               <div
                 className={this.state.activeTab === "profile" ? " active" : ""}
               >
-                <h2>{t("settings.profile")}</h2>
+                <div className="first_panel">
+                  <h2>{t("settings.profile")}</h2>
+                  <form
+                    method="post"
+                    action="/settings/submit"
+                    onSubmit={this.submit}
+                  >
+                    <span>{t("settings.username")}</span>
+                    <input
+                      type="text"
+                      name="username"
+                      className="Username-field"
+                      onChange={(e) =>
+                        this.setState({ email: e.target.value.trim() })
+                      }
+                    />
+                    <span>{t("settings.favourite_categories")}</span>
+                    <Select
+                      closeMenuOnSelect={false}
+                      components={animatedComponents}
+                      isMulti
+                      options={options}
+                    />
+                    <span className="bio">{t("settings.bio")}</span>
+                    <textarea
+                      type="text"
+                      name="bio"
+                      className="Bio-field"
+                      onChange={(e) =>
+                        this.setState({ message: e.target.value.trim() })
+                      }
+                    />
+                    <span>{t("settings.location")}</span>
+                    <textarea
+                      type="text"
+                      name="bio"
+                      className="Bio-field"
+                      onChange={(e) =>
+                        this.setState({ message: e.target.value.trim() })
+                      }
+                    />
+                    <input
+                      type="submit"
+                      className="Submit-button"
+                      value={t("settings.update.profile")}
+                    />
+                  </form>
+                </div>
+                <div className="photo">hello</div>
               </div>
               <div
                 className={this.state.activeTab === "security" ? " active" : ""}
               >
-                <h2>{t("settings.security")}</h2>
-                <form
-                  method="post"
-                  action="/settings/submit"
-                  onSubmit={this.submit}
-                >
-                  <span>{t("settings.email")}</span>
-                  <Input
-                    type="email"
-                    name="email"
-                    class="Email-field"
-                    onChange={(e) =>
-                      this.setState({ email: e.target.value.trim() })
-                    }
-                  />
-                  <h4>{t("settings.password.change")}</h4>
-                  <span>{t("settings.password.old")}</span>
-                  <Input
-                    type="password"
-                    name="old_password"
-                    class="Password-field"
-                    onChange={(e) =>
-                      this.setState({ username: e.target.value.trim() })
-                    }
-                  />
-                  <span>{t("settings.password.new")}</span>
-                  <Input
-                    type="password"
-                    name="new_password"
-                    class="Password-field"
-                    onChange={this.setPassword}
-                  />
-                  <span>{t("settings.password.confirm_new")}</span>
-                  <Input
-                    type="password"
-                    name="confirm_new_password"
-                    class="Password-field"
-                    onChange={this.setPasswordConfirm}
-                  />
-                  <p
-                    className={`Password-mismatch${
-                      this.state.mismatch ? "" : " hidden"
-                    }`}
-                    children={t("auth.signup.mismatch")}
-                  />
-                  <input
-                    type="submit"
-                    className="Submit-button"
-                    value={t("settings.update")}
-                  />
-                </form>
+                <div className="first_panel">
+                  <h2>{t("settings.security")}</h2>
+                  <form
+                    method="post"
+                    action="/settings/submit"
+                    onSubmit={this.submit}
+                  >
+                    <span>{t("settings.email")}</span>
+                    <input
+                      type="email"
+                      name="email"
+                      className="Email-field"
+                      onChange={(e) =>
+                        this.setState({ email: e.target.value.trim() })
+                      }
+                    />
+                    <h4>{t("settings.password.change")}</h4>
+                    <span>{t("settings.password.old")}</span>
+                    <input
+                      type="password"
+                      name="old_password"
+                      className="Password-field"
+                      onChange={(e) =>
+                        this.setState({ username: e.target.value.trim() })
+                      }
+                    />
+                    <span>{t("settings.password.new")}</span>
+                    <input
+                      type="password"
+                      name="new_password"
+                      className="Password-field"
+                      onChange={this.setPassword}
+                    />
+                    <span>{t("settings.password.confirm_new")}</span>
+                    <input
+                      type="password"
+                      name="confirm_new_password"
+                      className="Password-field"
+                      onChange={this.setPasswordConfirm}
+                    />
+                    <p
+                      className={`Password-mismatch${
+                        this.state.mismatch ? "" : " hidden"
+                      }`}
+                      children={t("settings.password.mismatch")}
+                    />
+                    <input
+                      type="submit"
+                      className="Submit-button"
+                      value={t("settings.update.security")}
+                    />
+                  </form>
+                </div>
               </div>
             </div>
           </div>
