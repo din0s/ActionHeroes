3ff2f2bf48545e4a6c6fa24156ccbbfd8c1e4d87
Christina <chriskreza@csd.auth.gr>
2020-05-13 11:33:55 +0300

    Created functions to handle update

--- src/api/controllers/ActionUserController.js
+++ src/api/controllers/ActionUserController.js
@@ -4,65 +4,85 @@ const mongo = require("mongoose");
 
 mongo.set("useFindAndModify", false);
 
+const updateAttendants = (req, res, add) => {
+  const userId = req.userData.userId;
+  const actionId = req.params.action_id;
+  var actionQuery = {};
+  var userQuery = {};
+
+  if (add) {
+    actionQuery["$addToSet"] = { attendees: userId };
+    userQuery["$addToSet"] = { actionsAttended: actionId };
+  } else {
+    actionQuery["$pull"] = { actionsAttended: actionId };
+    userQuery["$pull"] = { actionsAttended: actionId };
+  }
+
+  Action.findOneAndUpdate({ _id: actionId }, actionQuery)
+    .then((action) => {
+      if (action) {
+        User.findOneAndUpdate({ _id: userId }, userQuery)
+          .then(() => {
+            return res.status(201).send();
+          })
+          .catch((err) => {
+            console.error(`Error during user find():\n${err}`);
+            return res.status(500).send();
+          });
+      } else {
+        return res.status(400).json({ error: "This action doesn't exist" });
+      }
+    })
+    .catch(() => {
+      return res.status(400).send({ error: "Invalid action id" });
+    });
+};
+
+const updateSavedActions = (req, res, add) => {
+  const userId = req.userData.userId;
+  const actionId = req.params.action_id;
+
+  var userQuery = {};
+  if (add) {
+    userQuery["$addToSet"] = { actionsSaved: actionId };
+  } else {
+    userQuery["$pull"] = { actionsSaved: actionId };
+  }
+
+  Action.findById(actionId)
+    .then((action) => {
+      if (action) {
+        User.findOneAndUpdate({ _id: userId }, userQuery)
+          .then(() => {
+            return res.status(201).send();
+          })
+          .catch((err) => {
+            console.error(`Error during user find():\n${err}`);
+            return res.status(500).send();
+          });
+      } else {
+        return res.status(400).json({ error: "This action doesn't exist" });
+      }
+    })
+    .catch(() => {
+      return res.status(400).send({ error: "Invalid action id" });
+    });
+};
+
 module.exports = {
   addAttendant: (req, res) => {
-    const userId = req.userData.userId;
-    const actionId = req.params.action_id;
-
-    Action.findOneAndUpdate(
-      { _id: actionId },
-      { $addToSet: { attendees: userId } }
-    )
-      .then((action) => {
-        if (action) {
-          User.findOneAndUpdate(
-            { _id: userId },
-            { $addToSet: { actionsAttended: actionId } }
-          )
-            .then(() => {
-              return res.status(201).send();
-            })
-            .catch((err) => {
-              console.error(`Error during user find():\n${err}`);
-              return res.status(500).send();
-            });
-        } else {
-          return res.status(400).json({ error: "This action doesn't exist" });
-        }
-      })
-      .catch(() => {
-        return res.status(400).send({ error: "Invalid action id" });
-      });
+    updateAttendants(req, res, true);
   },
 
   addSavedAction: (req, res) => {
-    const userId = req.userData.userId;
-    const actionId = req.params.action_id;
-
-    Action.findById(actionId)
-      .then((action) => {
-        if (action) {
-          User.findOneAndUpdate(
-            { _id: userId },
-            { $addToSet: { actionsSaved: actionId } }
-          )
-            .then(() => {
-              return res.status(201).send();
-            })
-            .catch((err) => {
-              console.error(`Error during user find():\n${err}`);
-              return res.status(500).send();
-            });
-        } else {
-          return res.status(400).json({ error: "This action doesn't exist" });
-        }
-      })
-      .catch(() => {
-        return res.status(400).send({ error: "Invalid action id" });
-      });
+    updateSavedActions(req, res, true);
   },
 
-  removeAttendant: (req, res) => {},
+  removeAttendant: (req, res) => {
+    updateAttendants(req, res, false);
+  },
 
-  removeSavedAction: (req, res) => {},
+  removeSavedAction: (req, res) => {
+    updateSavedActions(req, res, false);
+  },
 };
