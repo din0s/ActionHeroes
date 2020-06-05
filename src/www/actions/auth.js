47465e0cd8ded8f9d33cf2c6a33ca3694fb3a3a9
Dinos <papakosk@csd.auth.gr>
2020-06-06 00:00:31 +0300

    Remove outdated console logs

--- src/www/actions/auth.js
+++ src/www/actions/auth.js
@@ -11,9 +11,7 @@ const setToken = (token) => {
 };
 
 const handleResponse = (dispatch, data) => {
-  console.log(data);
   if (data.error) {
-    console.log(data.error);
     dispatch({
       type: "ERROR",
       error: data.error,
