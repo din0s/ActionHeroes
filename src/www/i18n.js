9993076f8cefa245eb09b7fb96bc4953ccedea78
Dinos <papakosk@csd.auth.gr>
2020-06-05 23:51:29 +0300

    Update i18n

--- src/www/i18n.js
+++ src/www/i18n.js
@@ -1,18 +1,17 @@
-import Backend from "i18next-xhr-backend";
+import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 // more info: https://react.i18next.com/latest/using-with-hooks
 i18n
-  .use(Backend) // https://github.com/i18next/i18next-xhr-backend
+  .use(Backend) // https://github.com/i18next/i18next-http-backend
   .use(LanguageDetector) // https://github.com/i18next/i18next-browser-languageDetector
   .use(initReactI18next)
   .init({
     // all options: https://www.i18next.com/overview/configuration-options
     fallbackLng: "en",
-    whitelist: ["el", "en"],
-    debug: true
+    whitelist: ["el", "en"]
   });
 
 export default i18n;
