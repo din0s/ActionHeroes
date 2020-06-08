75b5d236cbf7f1391d395a1dea998752ee37af9c
Dinos <papakosk@csd.auth.gr>
2020-06-08 19:18:35 +0300

    Fix actions page loading without categories

--- src/www/containers/actionspage/ActionsPage.jsx
+++ src/www/containers/actionspage/ActionsPage.jsx
@@ -8,6 +8,7 @@ import SearchBar from "../../components/searchbar/SearchBar";
 import { connect } from "react-redux";
 import queryString from "query-string";
 import { withRouter } from "react-router-dom";
+import { withTranslation } from "react-i18next";
 
 const actions = require("./actions.json");
 
@@ -20,102 +21,108 @@ export default connect(
   undefined
 )(
   withRouter(
-    class ActionsPage extends Component {
-      state = {
-        categories: [],
-        selectedCategories: [],
-        query: "",
-      };
+    withTranslation()(
+      class ActionsPage extends Component {
+        state = {
+          categories: [],
+          selectedCategories: [],
+          query: "",
+        };
 
-      componentDidMount = () => {
-        const { query } = queryString.parse(this.props.location.search);
-        if (query) {
-          this.setState({ query });
-        }
-        this.setState({
-          categories: this.props.categories.map(c => c.name)
-        })
-      };
+        componentDidMount = () => {
+          const { query } = queryString.parse(this.props.location.search);
+          if (query) {
+            this.setState({ query });
+          }
+          this.setState({
+            categories: this.props.categories.map((c) => c.name),
+          });
+        };
 
-      onCheckbox = (event, category) => {
-        if (event.target.checked) {
-          const categories = this.state.selectedCategories.concat(category);
-          this.setState({ selectedCategories: categories });
-        } else {
-          this.removeCategory(category);
-        }
-      };
+        onCheckbox = (event, category) => {
+          if (event.target.checked) {
+            const categories = this.state.selectedCategories.concat(category);
+            this.setState({ selectedCategories: categories });
+          } else {
+            this.removeCategory(category);
+          }
+        };
 
-      removeCategory = (category) => {
-        const filtered = this.state.selectedCategories.filter(
-          (c) => c !== category
-        );
-        this.setState({
-          selectedCategories: filtered,
-        });
-      };
+        removeCategory = (category) => {
+          const filtered = this.state.selectedCategories.filter(
+            (c) => c !== category
+          );
+          this.setState({
+            selectedCategories: filtered,
+          });
+        };
 
-      showActions = (key, action) => {
-        const action_link = "/actions/id";
-        const location_link = "https://www.google.com/maps";
-        return (
-          <li key={key}>
-            <a href={action_link}>
-              <img alt="Card icon" src={action.logo} />
-            </a>
-            <div>
+        showActions = (key, action) => {
+          const action_link = "/actions/id";
+          const location_link = "https://www.google.com/maps";
+          return (
+            <li key={key}>
               <a href={action_link}>
-                <h3>{action.name}</h3>
+                <img alt="Card icon" src={action.logo} />
               </a>
-              <div className="date">
-                <span>{action.date}</span>
+              <div>
+                <a href={action_link}>
+                  <h3>{action.name}</h3>
+                </a>
+                <div className="date">
+                  <span>{action.date}</span>
+                </div>
+                <a
+                  href={location_link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span>{action.location}</span>
+                </a>
+                <div className="description">
+                  <p>{action.description}</p>
+                </div>
               </div>
-              <a href={location_link} target="_blank" rel="noopener noreferrer">
-                <span>{action.location}</span>
-              </a>
-              <div className="description">
+              <div className="hidden_desc">
                 <p>{action.description}</p>
               </div>
-            </div>
-            <div className="hidden_desc">
-              <p>{action.description}</p>
-            </div>
-          </li>
-        );
-      };
+            </li>
+          );
+        };
 
-      render() {
-        return (
-          <div className="ActionsPage">
-            <SearchBar
-              action="/actions"
-              value={this.state.query}
-              onChange={(e) => this.setState({ query: e.target.value })}
-            />
-            <span>
-              <FilterList
-                categories={this.state.categories}
-                selected={this.state.selectedCategories}
-                onCheckbox={this.onCheckbox}
-                onClear={() => this.setState({ selectedCategories: [] })}
-                onRemove={this.removeCategory}
+        render() {
+          return (
+            <div className="ActionsPage">
+              <SearchBar
+                action="/actions"
+                value={this.state.query}
+                onChange={(e) => this.setState({ query: e.target.value })}
               />
-              <Pagination
-                baseName="ActionsPage_content"
-                collection={actions}
-                perPage={6}
-                query={this.state.query.toLowerCase().trim()}
-                mapFunc={this.showActions}
-                searchFilter={(actions, query) =>
-                  actions.name.toLowerCase().includes(query) ||
-                  actions.description.toLowerCase().includes(query)
-                }
-                selected={this.state.selectedCategories}
-              />
-            </span>
-          </div>
-        );
+              <span>
+                <FilterList
+                  categories={this.state.categories}
+                  selected={this.state.selectedCategories}
+                  onCheckbox={this.onCheckbox}
+                  onClear={() => this.setState({ selectedCategories: [] })}
+                  onRemove={this.removeCategory}
+                />
+                <Pagination
+                  baseName="ActionsPage_content"
+                  collection={actions}
+                  perPage={6}
+                  query={this.state.query.toLowerCase().trim()}
+                  mapFunc={this.showActions}
+                  searchFilter={(actions, query) =>
+                    actions.name.toLowerCase().includes(query) ||
+                    actions.description.toLowerCase().includes(query)
+                  }
+                  selected={this.state.selectedCategories}
+                />
+              </span>
+            </div>
+          );
+        }
       }
-    }
+    )
   )
 );
