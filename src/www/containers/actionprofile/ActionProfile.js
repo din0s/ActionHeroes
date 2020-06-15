bb6399219dd5dc99cae3fbb59f41f519ecdc08f2
Christina <chriskreza@csd.auth.gr>
2020-06-15 22:53:26 +0300

    Connected save/attend buttons with the API

--- src/www/containers/actionprofile/ActionProfile.js
+++ src/www/containers/actionprofile/ActionProfile.js
@@ -7,158 +7,194 @@ import { parseDate } from "../../date";
 import { withTranslation } from "react-i18next";
 import axios from "axios";
 import { withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default withTranslation()(
-  withRouter(
-    class ActionProfile extends Component {
-      state = {
-        attendees: 0,
-        categories: [],
-        date: undefined,
-        isHost: false,
-        location: {},
-        name: "",
-        organizer: "",
-        saves: 0,
-        saved: false,
-        toAttend: false,
-        error: false,
-      };
+const mapState = (state) => ({
+  loggedIn: state.auth.loggedIn,
+});
 
-      componentDidMount = () => {
-        axios
-          .get(`/api/actions/${this.props.match.params.id}`)
-          .then((res) => {
-            this.setState({
-              ...res.data,
+export default connect(
+  mapState,
+  undefined
+)(
+  withTranslation()(
+    withRouter(
+      class ActionProfile extends Component {
+        state = {
+          id: "",
+          attendees: 0,
+          categories: [],
+          date: undefined,
+          isHost: false,
+          location: {},
+          name: "",
+          organizer: "",
+          saves: 0,
+          saved: false,
+          toAttend: false,
+          error: false,
+        };
+
+        componentDidMount = () => {
+          const { id } = this.props.match.params;
+          axios
+            .get(`/api/actions/${id}`)
+            .then((res) => {
+              this.setState({
+                ...res.data,
+                id,
+              });
+            })
+            .catch(() => {
+              this.setState({ error: true });
             });
-          })
-          .catch(() => {
-            this.setState({ error: true });
-          });
-      };
+        };
 
-      render() {
-        if (this.state.error) {
-          return <Redirect to="/actions/" />;
-        }
+        handleSubmit = (endpoint, field) => {
+          const url = `/api/actions/${this.state.id}/${endpoint}`;
+          if (this.state[field]) {
+            axios.delete(url).then(() => {
+              this.setState({ [field]: false });
+            });
+          } else {
+            axios.post(url).then(() => {
+              this.setState({ [field]: true });
+            });
+          }
+        };
 
-        const { t } = this.props;
-        const {
-          name,
-          date,
-          photo,
-          location,
-          description,
-          saves,
-          attendees,
-          categories,
-          organizer,
-        } = this.state;
+        handleSave = () => {
+          this.handleSubmit("save", "saved");
+        };
 
-        const actionPhoto = photo
-          ? `/api/images/${photo}`
-          : "/img/actionprofile/default.jpg";
+        handleAttend = () => {
+          this.handleSubmit("attend", "toAttend");
+        };
 
-        const organizerPhoto = organizer.photo
-          ? `/api/images/${organizer.photo}`
-          : "/img/teaminfo/default.png";
+        render() {
+          if (this.state.error) {
+            return <Redirect to="/actions/" />;
+          }
 
-        const coordinates = this.state.location.coordinates;
+          const { t } = this.props;
+          const {
+            name,
+            date,
+            photo,
+            location,
+            description,
+            saves,
+            attendees,
+            categories,
+            organizer,
+          } = this.state;
 
-        return (
-          <div className="ActionProfile">
-            <div className="ActionHeader">
-              <Parallax
-                blur={{ min: 0, max: 5 }}
-                className="ActionHeader_parallax"
-                bgImage={actionPhoto}
-                bgClassName="ActionHeader_parallaxPhoto"
-                bgImageAlt="Parallax"
-                strength={300}
-              >
-                <div className="ActionHeader_infoDiv">
-                  <img src={actionPhoto} alt="" />
-                  <div>
-                    <h1>{name}</h1>
-                    <p>{parseDate(date, t)}</p>
-                    <button
-                      onClick={() =>
-                        this.setState({ saved: !this.state.saved })
-                      }
-                      children={
-                        this.state.saved
-                          ? t("actioninfo.unsave")
-                          : t("actioninfo.save")
-                      }
-                    />
-                    <button
-                      onClick={() =>
-                        this.setState({ toAttend: !this.state.toAttend })
-                      }
-                      children={
-                        this.state.toAttend
-                          ? t("actioninfo.cancel")
-                          : t("actioninfo.attend")
-                      }
-                    />
-                  </div>
-                </div>
-              </Parallax>
-            </div>
-            <div className="ActionDetails">
-              <h3>{t("actioninfo.about")}</h3>
-              <div className="ActionDetails_panel">
-                <div className="ActionDetails_infoDiv">
-                  <span>
-                    <img src={organizerPhoto} alt="Organizer"></img>
+          const actionPhoto = photo
+            ? `/api/images/${photo}`
+            : "/img/actionprofile/default.jpg";
+
+          const organizerPhoto = organizer.photo
+            ? `/api/images/${organizer.photo}`
+            : "/img/teaminfo/default.png";
+
+          const coordinates = this.state.location.coordinates;
+
+          return (
+            <div className="ActionProfile">
+              <div className="ActionHeader">
+                <Parallax
+                  blur={{ min: 0, max: 5 }}
+                  className="ActionHeader_parallax"
+                  bgImage={actionPhoto}
+                  bgClassName="ActionHeader_parallaxPhoto"
+                  bgImageAlt="Parallax"
+                  strength={300}
+                >
+                  <div className="ActionHeader_infoDiv">
+                    <img src={actionPhoto} alt="" />
                     <div>
-                      <h3> {organizer.name}</h3>
-                      <p>{t("actioninfo.organizer")}</p>
-                    </div>
-                  </span>
-                  <div>
-                    <p className="ActionDetails_infoDiv_location">
-                      {location.name}
-                    </p>
-                    <p
-                      className="ActionDetails_infoDiv_map"
-                      onClick={() => {
-                        window.open(
-                          "https://maps.google.com?q=" +
-                            `${coordinates[0]}` +
-                            "," +
-                            `${coordinates[1]}`
-                        );
-                      }}
-                    >
-                      {"📍  " + t("actioninfo.map")}
-                    </p>
-                    <p>{description}</p>
-                    <p className="Attendees">
-                      {attendees + saves + t("actioninfo.attendees")}
-                    </p>
-                    <div className="ActionDetails_infoDiv_categories">
-                      <h3>{t("actioninfo.tags")}</h3>
-                      <ul>
-                        {categories.map((c) => {
-                          const category = c.toLowerCase();
-                          return <li key={c}>{t(`categories.${category}`)}</li>;
-                        })}
-                      </ul>
+                      <h1>{name}</h1>
+                      <p>{parseDate(date, t)}</p>
+                      {this.props.loggedIn && (
+                        <div>
+                          <button
+                            onClick={this.handleSave}
+                            children={
+                              this.state.saved
+                                ? t("actioninfo.unsave")
+                                : t("actioninfo.save")
+                            }
+                          />
+                          <button
+                            onClick={this.handleAttend}
+                            children={
+                              this.state.toAttend
+                                ? t("actioninfo.cancel")
+                                : t("actioninfo.attend")
+                            }
+                          />
+                        </div>
+                      )}
                     </div>
                   </div>
-                </div>
-                <div className="ActionDetails_organizerDiv">
-                  <img src={organizerPhoto} alt="Organizer"></img>
-                  <h3> {organizer.name}</h3>
-                  <p>{t("actioninfo.organizer")}</p>
+                </Parallax>
+              </div>
+              <div className="ActionDetails">
+                <h3>{t("actioninfo.about")}</h3>
+                <div className="ActionDetails_panel">
+                  <div className="ActionDetails_infoDiv">
+                    <span>
+                      <img src={organizerPhoto} alt="Organizer"></img>
+                      <div>
+                        <h3> {organizer.name}</h3>
+                        <p>{t("actioninfo.organizer")}</p>
+                      </div>
+                    </span>
+                    <div>
+                      <p className="ActionDetails_infoDiv_location">
+                        {location.name}
+                      </p>
+                      <p
+                        className="ActionDetails_infoDiv_map"
+                        onClick={() => {
+                          window.open(
+                            "https://maps.google.com?q=" +
+                              `${coordinates[0]}` +
+                              "," +
+                              `${coordinates[1]}`
+                          );
+                        }}
+                      >
+                        {"📍  " + t("actioninfo.map")}
+                      </p>
+                      <p>{description}</p>
+                      <p className="Attendees">
+                        {attendees + saves + t("actioninfo.attendees")}
+                      </p>
+                      <div className="ActionDetails_infoDiv_categories">
+                        <h3>{t("actioninfo.tags")}</h3>
+                        <ul>
+                          {categories.map((c) => {
+                            const category = c.toLowerCase();
+                            return (
+                              <li key={c}>{t(`categories.${category}`)}</li>
+                            );
+                          })}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ActionDetails_organizerDiv">
+                    <img src={organizerPhoto} alt="Organizer"></img>
+                    <h3> {organizer.name}</h3>
+                    <p>{t("actioninfo.organizer")}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
+          );
+        }
       }
-    }
+    )
   )
 );
