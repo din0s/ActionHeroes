13c6f758d054e19dc54f5de04cad8cd665540651
GregB712 <gregorybarbas@gmail.com>
2020-06-19 16:41:09 +0300

    Addition of Social Media in Footer

--- src/www/components/footer/Footer.js
+++ src/www/components/footer/Footer.js
@@ -3,15 +3,62 @@ import "./Footer.scss";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+const fb_icon = "/img/social_media/facebook.svg";
+const insta_icon = "/img/social_media/instagram.svg";
+const twitter_icon = "/img/social_media/twitter.svg";
+const linkedin_icon = "/img/social_media/linkedin.svg";
+const github_icon = "/img/social_media/github.svg";
+
 export default function Footer() {
   const { t } = useTranslation();
   return (
     <footer>
       <hr className="Divider" />
-      <p>&copy; ActionHeroes 2020</p>
-      <p>
-        {t("footer.icons", { site: "flaticon.com", site2: "unsplash.com" })}
-      </p>
+      <div className = "Content">
+        <div className="Copyrights">
+          <p>&copy; ActionHeroes 2020</p>
+          <p>
+            {t("footer.icons", { site: "flaticon.com", site2: "unsplash.com" })}
+          </p>
+        </div>
+        <div className="Social_Media">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com"
+          >
+            <img alt="fb_icon" src={fb_icon} />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com"
+          >
+            <img alt="insta_icon" src={insta_icon} />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.twitter.com"
+          >
+            <img alt="twitter_icon" src={twitter_icon} />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com"
+          >
+            <img alt="linkedin_icon" src={linkedin_icon} />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/din0s/ActionHeroes/"
+          >
+            <img alt="github_icon" src={github_icon} />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
